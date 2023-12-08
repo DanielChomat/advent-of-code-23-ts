06e49946256e0b4f165af9ae0d9918927758be32
Daniel Chomát <daniel@chomat.eu>
2023-12-08 14:18:21 +0100

    Feat: Day 4: Scratchcards (p1)

---
+++ src/04/p1/script.ts
@@ -0,0 +1,57 @@
+import lineReader from "line-reader";
+
+const values: number[] = []
+
+const findFirstAndLastDigit = (line: string): number => {
+    const splitLine = line.split(':')
+
+    const allNumbersString = splitLine[1]
+    const splitNumbers = allNumbersString.split('|')
+
+    const winningNumbersString = splitNumbers[0]
+    const acquiredNumbersString = splitNumbers[1]
+
+    // TODO: Fix this empty array?
+    const winningNumbers = winningNumbersString.match(/\d+/g) ?? []
+    const acquiredNumbers = new Set(acquiredNumbersString.match(/\d+/g))
+
+
+    const multipliedNumber = winningNumbers.reduce((acc, curr) => {
+        const hasWinningNumber = acquiredNumbers.has(curr)
+
+        if (!hasWinningNumber) return acc;
+
+        console.log({hasWinningNumber})
+        console.log({curr})
+        console.log({acc})
+
+        const add = acc === 0 ? 1 : 2;
+        const newAcc = acc * add;
+
+        return newAcc;
+    }, 0)
+
+    if (multipliedNumber > 0) {
+        console.log({winningNumbers})
+        console.log({acquiredNumbers})
+    }
+
+    console.log({ multipliedNumber})
+
+
+    return multipliedNumber;
+}
+
+const logTheResult = () => {
+    const result = values?.reduce((acc, curr) => acc + curr, 0)
+
+    console.log({result})
+}
+
+lineReader.eachLine('./src/04/input.txt', function (line, last) {
+    const digit = findFirstAndLastDigit(line);
+
+    values.push(digit);
+
+    if (last) logTheResult()
+});
